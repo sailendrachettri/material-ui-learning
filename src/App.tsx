0ac21c220e3573aeb0d3e26e810327a2d13fcbda
sailendrachettri <sailendra9083@gmail.com>
2023-07-04 12:41:12 +0530

    image list

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
 import React from 'react';
 import './App.css';
-import MuiAccordian from './components/MuiAccordian';
-// import { MuiCard } from './components/MuiCard';
-// import { Muipaper } from './components/Muipaper';
-// import { MuiGrid } from './components/MuiGrid';
-// import { MuiStack } from './components/MuiStack';
-// import { MuiBox } from './components/MuiBox';
-// import MuiAutocomplete from './components/MuiAutocomplete';
-// import MuiRating from './components/MuiRating';
-// import MuiSwitch from './components/MuiSwitch';
-// import MuiCheckBox from './components/MuiCheckBox';
-// import MuiRadio from './components/MuiRadio';
-// import MuiSelect from './components/MuiSelect';
-// import { MuiTypography } from './components/MuiTypography';
-// import MuiButtons from './components/MuiButtons';
-// import MuiTextField from './components/MuiTextField';
+import MuiImageList from './components/MuiImageList';
 
 function App() {
   return (
     <>
       <div className="App">
-        {/* <MuiTypography /> */}
-        {/* <MuiButtons /> */}
-        {/* <MuiTextField /> */}
-        {/* <MuiSelect /> */}
-        {/* <MuiRadio /> */}
-        {/* <MuiCheckBox /> */}
-        {/* <MuiSwitch /> */}
-        {/* <MuiRating /> */}
-        {/* <MuiAutocomplete /> */}
-        {/* <MuiBox /> */}
-        {/* <MuiStack /> */}
-        {/* <MuiGrid /> */}
-        {/* <Muipaper /> */}
-        {/* <MuiCard /> */}
-        <MuiAccordian />
+        <MuiImageList />
       </div>
     </>
   );
