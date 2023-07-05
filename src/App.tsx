df0aecbed35a9718614190e29c621138e3d81062
sailendrachettri <sailendra9083@gmail.com>
2023-07-05 10:57:11 +0530

    loading button from mui/lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { MuiSkeleton } from './components/MuiSkeleton'
+import { MuiLoadingButton } from './components/MuiLoadingButton'
 
 
 function App() {
   return (
     <>
       <div className="App">
-        <MuiSkeleton />
+        <MuiLoadingButton />
       </div>
     </>
   );
--- src/components/MuiProgress.tsx
+++ src/components/MuiProgress.tsx
@@ -1,13 +1,14 @@
-import { LinearProgress, Stack } from '@mui/material'
+import { CircularProgress, LinearProgress, Stack } from '@mui/material'
 
 export const MuiProgress = () => {
+    // eslint-disable-next-line
     return (
         <>
-            {/* <Stack spacing={2} direction='row' marginTop={2}>
+            <Stack spacing={2} direction='row' marginTop={2}>
                 <CircularProgress />
                 <CircularProgress color='success' />
                 <CircularProgress color='secondary' value={50} variant='determinate' />
-            </Stack> */}
+            </Stack>
 
             <Stack spacing={2}>
                 <LinearProgress />
