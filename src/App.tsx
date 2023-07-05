35a18b2700400fc1ce5fcd1949667e6a37e358c0
sailendrachettri <sailendra9083@gmail.com>
2023-07-05 15:03:12 +0530

    mui color palette custom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import './App.css'
 import { MuiResponsiveness } from './components/MuiResponsiveness'
+import { createTheme, colors, ThemeProvider } from '@mui/material';
 
-
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e'
+  },
+  palette: {
+    secondary: {
+      main: colors.orange[500]
+    },
+    neutral: {
+      main: colors.grey[500],
+      darker: colors.grey[700]
+    }
+  }
+})
 function App() {
   return (
     <>
       <div className="App">
-        <MuiResponsiveness />
+        <ThemeProvider theme={theme}>
+          <MuiResponsiveness />
+        </ThemeProvider>
       </div>
     </>
   );
--- src/components/MuiResponsiveness.tsx
+++ src/components/MuiResponsiveness.tsx
@@ -1,20 +1,29 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import { styled } from '@mui/material'
+
+const StyledBox = styled(Box)(({ theme }) => ({
+    height: '250px',
+    width: '250px',
+    backgroundColor: theme.palette.neutral?.darker
+}))
 
 export const MuiResponsiveness = () => {
     return (
-        <Box sx={{
-            height: '300px',
-            width: {
-                sx: 100,
-                sm: 200,
-                md: 300,
-                lg: 400,
-                xl: 500
-            },
-            bgcolor: 'primary.main'
-        }}>
+        <>
+            <Box sx={{
+                height: '300px',
+                width: {
+                    sx: 100,
+                    sm: 200,
+                    md: 300,
+                    lg: 400,
+                    xl: 500
+                },
+                bgcolor: 'secondary.dark'
+            }}>
+            </Box>
 
-        </Box>
+            <StyledBox />
+        </>
     )
 }
